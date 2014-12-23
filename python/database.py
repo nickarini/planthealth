54c4c3af0c1aaede4513a17b9482c96bfe37d7cb
nickarini <nick.arini@gmail.com>
2014-12-23 15:38:43 +0000

    adding local db cache

--- python/database.py
+++ python/database.py
@@ -2,9 +2,21 @@
 
 import sqlite3
 
+
+def add_multi_reading (temp, humidity, lux):
+
+    curs.execute("INSERT INTO stats values(date('now'), time('now'), (?), (?), (?))", (temp, humidity, lux,))
+
+    # commit the changes
+    conn.commit()
+
+
+
 conn=sqlite3.connect('statslog.db')
 
 curs=conn.cursor()
+
+add_multi_reading(12.0, 68.3, 450.0)
 
 print "\nEntire database contents:\n"
 for row in curs.execute("SELECT * FROM stats"):
