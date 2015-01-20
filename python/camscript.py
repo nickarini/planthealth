ad01e362def3be66dc213982399019dcf6c01f4b
nickarini <nick.arini@gmail.com>
2015-01-20 17:05:34 +0000

    fixed camscript paths

--- python/camscript.py
+++ python/camscript.py
@@ -2,6 +2,7 @@ import datetime
 import time
 import picamera
 import subprocess
+import os, errno
 
 outputdir = '/home/pi/'  # outputdir must exist and be writable
 outputstats = 'ndvi_stats.csv'
@@ -23,9 +24,15 @@ while var == 1 :  # This constructs an infinite loop
         camera.stop_preview()
 
 
-    index = subprocess.check_output('../planthealth %s' % filename, shell=True)
+    index = subprocess.check_output('../c++/src/planthealth %s' % outputdir+filename, shell=True)
     f.write(index)
     print index 
+
+    # now delete the file so we dont fill up the disk (need to make this an optional
+    try:
+        os.remove(outputdir+filename)
+    except OSError:
+        pass
 
     time.sleep(60)
 
